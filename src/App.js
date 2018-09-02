f8c6203ce94321c799876bd2e01c5ae20bff15ff
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-09-02 11:59:30 -0700

    feat: add map area

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Fragment,Component } from 'react';
+import React, { Fragment,Component } from 'react'
 
-import './App.css';
+import FilterMenu from './Components/filter-menu.js'
+import Map from './Components/Map'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <h1 className="title"><span>New Orleans</span><br />Neighborhood Map</h1>
+        <div className="wrapper">
+          <FilterMenu />
+          <Map />
+        </div>
       </Fragment>
     )
   }
