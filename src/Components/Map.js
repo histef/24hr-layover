b5f84113ca441608d4d101e70f0f91b025c472b3
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-09-02 12:02:48 -0700

    chore: add Components folder for organization

---
+++ src/Components/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Map(){
+  return (
+    <div className='map'>
+    </div>
+  )
+}
+
+export default Map
---
+++ src/Components/filter-menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function FilterMenu(props){
+  return (
+    <div className='filter-menu'>
+      <h2>Filter Menu</h2>
+      <div className='dropdown'>
+        stuff <br /> more
+      </div>
+    </div>
+  )
+}
+
+export default FilterMenu
+
+// TODO: dropdown Menu
+// TODO: grab the markers
